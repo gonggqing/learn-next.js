3c4f6b54db0b916b1bc25d9a67a0d2d35d09a670
gongqing.dong <gongqing.dong@mthreads.com>
2023-12-04 19:16:52 +0800

    chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -38,7 +38,7 @@ export async function fetchLatestInvoices() {
   noStore();
   try {
     console.log('Fetching latest invoices...')
-    await new Promise((resolve) => setTimeout(resolve, 1500));
+    
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
